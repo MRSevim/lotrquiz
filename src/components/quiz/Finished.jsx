d02d198f0612b121cbab4255e4ba5b9b627b6262
MRSevim <lionrasit@gmail.com>
2024-09-05 15:23:24 +0300

    added margin y to finished

--- src/components/quiz/Finished.jsx
+++ src/components/quiz/Finished.jsx
@@ -3,13 +3,13 @@ export const Finished = () => {
   const progress = useSelector((state) => state.quiz.progress);
   const questions = useSelector((state) => state.quiz.questions);
   return (
-    <>
+    <div className="mx-2">
       <h1 className="my-2 text-xl font-bold">{progress.displayed.title}</h1>
       <p className="my-2 text-xl">
         You got {progress.correctQuestionCount} out of {questions.length}{" "}
         questions right.
       </p>
       <p>{progress.displayed.text}</p>
-    </>
+    </div>
   );
 };
